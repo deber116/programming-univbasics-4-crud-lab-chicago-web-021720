8d665aa5e5db023edcaa89d0bf0e2762634e0bb1
David Eber <deber116@gmail.com>
2020-01-28 22:56:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  dogs = ["corgi","poodle","lab","hound"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array
 end
 
 def remove_element_from_end_of_array(array)
